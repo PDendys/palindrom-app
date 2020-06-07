22fad158a1094dd7e0dd18b4a893f96149e74718
Piotr Dendys <piotr.dendys@quantoo.pl>
2020-06-07 16:11:33 +0200

    idea comments

--- src/components/login-form/LoginForm.tsx
+++ src/components/login-form/LoginForm.tsx
@@ -4,6 +4,20 @@ import { useForm } from 'react-hook-form';
 
 import { authenticateUser } from 'slices/AuthSlice';
 
+// HOW TO IMPROVE?:
+//  - Aplikacja opiera się jedynie na podstawowej frontendowej autentykacji, która nie jest w żadnym stopniu wystarczająco
+//    bezpieczna aby wykorzystywać ją w wersji produkcyjnej systemu.
+// - Powinna zostać zaimplementowana autentykacja backendowa.
+// - W przypadku architektury client - server backend powinien zwracać odpowiedni token (np. JWT), który będzie dołączany
+//   do każdego zapytania w celu potwierdzenia odpowiednich uprawnień użytkownika.
+// - W przypadku klasycznej architektury MVC (gdzie react odpowiada za warstwę widoku, a całość jest serwowana z tej samej domeny)
+//   aplikacja backendowa powinna zwracać ciasteczko sesji (najlepiej http only), które będzie dołączane przez przeglądarkę automatycznie do każdego zapytania.
+// - Powinna zostać zimplementowana funkcjonalność ograniczonej żywotności zarówno tokena JWT jak i ciasteczka sesji oraz sposób ich odzyskiwania.
+// - Aplikacja powinna być hostowana poprzez protokół https aby uzyskać szyfrowanie danych przesyłanych do backendu.
+// - W wypadku wrażliwych danych powinna zostać zaimplementowana funkcjonalność 2 stopniowej autentykacji.
+// - Jako że mamy do czynienia ze SPA powinna również zostać zaimplementowana funkcjonalność przechowywania
+//   stanu zalogowania użytkownika w pamięci podręcznej (np. cookies) w celu oddzyskania "zalogowania" po refreshu strony.
+
 const LoginForm: React.FC = () => {
     const { register, handleSubmit, errors } = useForm();
     const dispatch = useDispatch();
