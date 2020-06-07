46dd1fddd83db06ba26f87ea08e54c0b5b3e0375
Piotr Dendys <piotr.dendys@quantoo.pl>
2020-06-07 13:27:35 +0200

    inject login form into login page

---
+++ src/components/palindrom-checker-form/PalindromCheckerForm.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+
+import { addItem } from 'slices/PalindromSlice';
+
+import { checkIsPalindrome } from 'helpers';
+
+const PalindromCheckerForm: React.FC = () => {
+    const [sentence, setSentence] = useState<string>('');
+    const dispatch = useDispatch();
+    const { register, handleSubmit, errors } = useForm();
+
+    const onSubmit = (): void => {
+        const isPalindrome: boolean = checkIsPalindrome(sentence);
+        dispatch(
+            addItem({ sentence, isPalindrome })
+        );
+        setSentence('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="row">
+                <div className="col-12 mb-3 text-left">
+                    <label htmlFor="exampleInputEmail1">Your sentence</label>
+                    <input
+                        value={sentence}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSentence(e.target.value)}
+                        type="text"
+                        className="form-control"
+                        id="sentence"
+                        name="sentence"
+                        ref={register({ required: true })}
+                    />
+                    {errors.sentence && <small className="text-danger">This field is required</small>}
+                </div>
+                <div className="col-12 text-right">
+                    <button className="btn btn-primary btn-md px-4" type="submit">Check</button>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default PalindromCheckerForm;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { IStore } from 'types';
+
+import LoginForm from 'components/login-form/LoginForm';
 
 const LoginPage: React.FC = () => {
+    const isAuth = useSelector((state: IStore) => state.auth.isAuth);
+
+    if (isAuth) {
+        return <Redirect to="/checker" />;
+    }
+
     return (
         <div className="container mt-5">
             <div className="row">
                 <div className="col-12 col-md-6 offset-md-3">
-                    login form here
+                    <LoginForm />
                 </div>
             </div>
         </div>
